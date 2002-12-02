8e7e66542c96a0c05ee1bce0ef5db9bd9e3a72a9
gmane <gmane>
2002-12-02 00:51:14 +0000

    Export more functions.

--- mdb.h
+++ mdb.h
@@ -1,6 +1,8 @@
 #ifndef MDB_H
 #define MDB_H
 
+#include "tokenizer.h"
+
 #define BLOCK_HEADER_SIZE 8
 #define INSTANCE_BLOCK_HEADER_SIZE 4
 
@@ -24,5 +26,12 @@ word_descriptor *enter_word(char *word);
 word_descriptor *lookup_word(const char *word);
 void mdb_init(void);
 void mdb_report(void);
+void write_group_table(void);
+int enter_article(document *doc, char *group, int article);
+void enter_instance(unsigned int article_id, word_descriptor *wd,
+		    unsigned int count);
+int is_number(const char *string);
+
+#define ARTICLE_SIZE (MAX_SAVED_BODY_LENGTH + MAX_HEADER_LENGTH*2 + 4 + 4 + 4)
 
 #endif
