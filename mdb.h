7d49d8238fa5ac8fcbd846876e44d15219617881
gmane <gmane>
2002-12-03 23:36:48 +0000

    Define more functions.

--- mdb.h
+++ mdb.h
@@ -5,6 +5,7 @@
 
 #define BLOCK_HEADER_SIZE 8
 #define INSTANCE_BLOCK_HEADER_SIZE 4
+#define MAX_GROUP_NAME_LENGTH 1024
 
 typedef struct {
   const char *word;
@@ -26,12 +27,18 @@ word_descriptor *enter_word(char *word);
 word_descriptor *lookup_word(const char *word);
 void mdb_init(void);
 void mdb_report(void);
-void write_group_table(void);
+void flush_group_table(void);
+void flush(void);
 int enter_article(document *doc, char *group, int article);
 void enter_instance(unsigned int article_id, word_descriptor *wd,
 		    unsigned int count);
 int is_number(const char *string);
 
 #define ARTICLE_SIZE (MAX_SAVED_BODY_LENGTH + MAX_HEADER_LENGTH*2 + 4 + 4 + 4)
+
+void read_group_table(void);
+void read_next_article_id(void);
+void read_word_table(void);
+void read_next_instance_block_number(void);
 
 #endif
