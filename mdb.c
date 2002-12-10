19a2ac5b1cb78123d1ab38d5322b7786d45cf33a
larsi <larsi>
2002-12-10 10:49:32 +0000

    Functions for defragmenting the instance file.

--- mdb.c
+++ mdb.c
@@ -639,7 +639,7 @@ void enter_instance(unsigned int article_id, word_descriptor *wd,
 
 void mdb_init(void) {
   if ((instance_file = open64(index_file_name(INSTANCE_FILE),
-			      O_RDWR|O_CREAT|O_STREAMING, 0644)) == -1)
+			      O_RDWR|O_CREAT, 0644)) == -1)
     merror("Opening the instance file");
 
   /*
@@ -1338,5 +1338,109 @@ void dump_statistics(void) {
   printf("Total words: %d, total single-instance words: %d\n", 
 	 total_word_instances,
 	 total_single_word_instances);
+
+}
+
+
+/* De-fragment the instance table. */
+
+static int new_instance_block_number = 1;
+static int new_instance_file = 0;
+
+void defragment_word(word_descriptor *wd) {
+  instance_block *ib = get_instance_block(*wd->head);
+  int next;
+  int nblocks = 0;
+
+  *wd->head = new_instance_block_number;
+
+  while (ib) {
+    nblocks++;
+    if ((next = *(int*)(ib->block)) != 0) {
+      new_instance_block_number++;
+      *(int*)(ib->block) = new_instance_block_number;
+      write_from(new_instance_file, ib->block, BLOCK_SIZE);
+      ib = get_instance_block(next);
+    } else {
+      *wd->tail = new_instance_block_number;
+      new_instance_block_number++;
+      write_from(new_instance_file, ib->block, BLOCK_SIZE);
+      ib = NULL;
+    }
+  }
+
+  if (nblocks != 1)
+    printf("Wrote %d blocks for %s\n", nblocks, wd->word);
+}
+
+int defragment_word_block(char *word_block) {
+  const char *b = word_block + BLOCK_HEADER_SIZE; /* Skip past the header. */
+  const char *word;
+  static word_descriptor dword;
+  int nwords = 0;
+
+  dirty_block(word_block);
+
+  while (TRUE) {
+    if (*b == 0) {
+      /* We have reached the end of the block; check whether it's the
+	 last block. */
+      if (*((int*)word_block) != 0) {
+	/* printf("Going to the next block, %d.\n", *((int*)word_block)); */
+	word_block = word_extension_table[*((int*)word_block)];
+	if (word_block == NULL) {
+	  printf("Went to a non-existing block.\n");
+	  exit(1);
+	}
+	dirty_block(word_block);
+	b = word_block + BLOCK_HEADER_SIZE;
+      } else 
+	return nwords;
+    }
+
+    if (word_block == NULL) {
+      printf("Went to a non-existing block here.\n");
+      exit(1);
+    } 
+
+    nwords++;
+    word = b;
+    while (*b++)
+      ;
+
+    dword.word = word;
+    dword.word_id = *((int*)b);
+    b += 4;
+    dword.head = ((int*)b);
+    b += 4;
+    dword.tail = ((int*)b);
+    b += 4;
+    if (dword.head)
+      defragment_word(&dword);
+  }
+}
+
+
+void defragment_instance_table(void) {
+  int i;
+  char *block;
+  int nwords = 0;
+
+  new_instance_block_number = 1;
+
+  if ((new_instance_file = open64("/opt/tmp/instances.db.tmp",
+				  O_RDWR|O_CREAT|O_TRUNC, 0644)) == -1)
+    merror("Opening the instance file");
+
+  for (i = 0; i<WORD_SLOTS; i++) {
+    if ((block = (char*)word_table[i]) != NULL) {
+      nwords += defragment_word_block(block);
+      if (! (nwords % 100))
+	printf("Total words: %d\n", nwords);
+    }
+  }
+  printf("Total words: %d\n", nwords);
+
+  flush();
 
 }
