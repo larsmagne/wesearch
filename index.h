9e50253e151e1bc492b9701738bf2b40b865602a
larsi <larsi>
2003-03-30 19:54:59 +0000

    Define indexed_files.

--- index.h
+++ index.h
@@ -2,9 +2,12 @@
 #define INDEX_H
 
 #define MAX_FILE_NAME 1024
+#define INDEXED_FILES_FILE "indexed_files.db"
 
 void soft_flush(void);
 int index_file(const char *file_name);
+void indexer_init(void);
+void read_indexed_files_table(void);
 
 extern int total_unique_words;
 extern int total_files;
@@ -12,6 +15,6 @@ extern time_t start_time;
 extern char *news_spool;
 extern char *from_file;
 extern int instances;
-
+extern int suppress_duplicate_files;
 
 #endif
