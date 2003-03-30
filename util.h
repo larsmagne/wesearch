de4741d0fbaaed7742c202e1f0dc6fb86e47e4cb
larsi <larsi>
2003-03-30 19:56:26 +0000

    Export functions.

--- util.h
+++ util.h
@@ -8,5 +8,7 @@ loff_t file_size (int fd);
 char *cmalloc(int size);
 int write_from(int fp, char *buf, int size);
 void merror(char *error);
+void read_into(int fd, int block_id, char *block, int block_size);
+void read_block(int fd, char *block, int block_size);
 
 #endif
