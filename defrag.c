b7ed39178b5eb9c03b7c79918800f8072faa25d2
larsi <larsi>
2003-10-25 21:39:12 +0000

    Do work on a better defragmentation routine.

--- defrag.c
+++ defrag.c
@@ -105,7 +105,6 @@ void defragment_instance_table(void) {
   int i = 0;
   char *block;
   int nwords = 0;
-  int id = 1;
 
   new_instance_block_number = 1;
 
@@ -180,7 +179,7 @@ instance_order *find_instance_order(void) {
   return instance_chain;
 }
 
-void sort_instance_chain(instance_order *instance_chain) {
+void old_sort_instance_chain(instance_order *instance_chain) {
   int i;
   int new_id = 0;
   instance_order *io;
@@ -199,3 +198,208 @@ void sort_instance_chain(instance_order *instance_chain) {
   }
 }
 
+
+
+typedef struct {
+  char *word_entry;
+  int next;
+} instance_chain;
+
+typedef struct {
+  int id;
+  int location;
+} instance_sort;
+
+int supposed_to_be[INSTANCE_TABLE_SIZE];
+instance_sort where_it_is[INSTANCE_TABLE_SIZE];
+instance_chain ic[INSTANCE_TABLE_SIZE];
+
+void read_instance_chain(void) {
+  char *buffer = NULL;
+  int nblocks = 1024;
+  int i = 0;
+  int next;
+  int instance_file;
+
+  if ((instance_file = open64(index_file_name(INSTANCE_FILE),
+			      O_RDWR|O_CREAT, 0644)) == -1)
+    merror("Opening the instance file");
+
+  buffer = cmalloc(nblocks * BLOCK_SIZE);
+
+  while (i < current_instance_block_number) {
+    if (! (i % nblocks)) 
+      read_block(instance_file, buffer, nblocks * BLOCK_SIZE);
+
+    next = (int)(buffer[(i % nblocks) * BLOCK_SIZE]);
+    if (next != 0)
+      ic[i].next = next;
+    i++;
+  }
+
+  close(instance_file);
+}
+
+void fill_instance_chain_with_word_block(char *word_block) {
+  char *b = word_block + BLOCK_HEADER_SIZE; /* Skip past the header. */
+  int head;
+
+  while (TRUE) {
+    if (*b == 0) {
+      /* We have reached the end of the block; check whether it's the
+	 last block. */
+      if (*((int*)word_block) != 0) {
+	/* printf("Going to the next block, %d.\n", *((int*)word_block)); */
+	word_block = word_extension_table[*((int*)word_block)];
+	if (word_block == NULL) {
+	  printf("Went to a non-existing block.\n");
+	  exit(1);
+	}
+	b = word_block + BLOCK_HEADER_SIZE;
+      } else 
+	return;
+    }
+
+    if (word_block == NULL) {
+      printf("Went to a non-existing block here.\n");
+      exit(1);
+    } 
+
+    while (*b++)
+      ;
+
+    printf("Word: %s\n", b);
+
+    b += 4;
+    head = *((int*)b);
+    ic[head].word_entry = b;
+    b += 4;
+    b += 4;
+  }
+}
+
+void fill_instance_chain_with_word_entries(void) {
+  int i;
+  char *block;
+
+  for (i = 0; i<WORD_SLOTS; i++) {
+    if ((block = (char*)word_table[i]) != NULL) {
+      fill_instance_chain_with_word_block(block);
+    }
+  }
+}
+
+void sort_instance_chain (void) {
+  int search = 1, next;
+  int i;
+
+  bzero((char*)supposed_to_be, sizeof(int)*INSTANCE_TABLE_SIZE);
+
+  for (i = 1; i<current_instance_block_number; i++) {
+    next = search + 1;
+    if (! supposed_to_be[search]) {
+      supposed_to_be[i] = search;
+      while (ic[search].next) 
+	search = ic[search].next;
+    } else {
+      search++;
+    }
+  }
+
+  /* Initialize where_it_is as well. */
+  for (i = 0; i<current_instance_block_number; i++) {
+    where_it_is[i].id = i;
+  }  
+}
+
+int min (int one, int two) {
+  if (one < two)
+    return one;
+  else
+    return two;
+}
+
+int sort_buffer_less(const void *sr1, const void *sr2) {
+  return (supposed_to_be[((instance_sort*)sr1)->id] - 
+	  supposed_to_be[((instance_sort*)sr2)->id]);
+}
+
+void sort_buffer(int length, int start_id) {
+  int i;
+
+  for (i = 0; i<length; i++) 
+    where_it_is[start_id + i].location = i;
+
+  qsort(where_it_is + start_id, sizeof(instance_sort), length, sort_buffer_less);
+}
+
+void save_sorted(int fd, char* buffer, int length, int start_id) {
+  int i;
+
+  for (i = 0; i<length; i++) 
+    write_from(fd, buffer + where_it_is[start_id + i].location * BLOCK_SIZE, BLOCK_SIZE);
+}
+
+void move_sorted(char* buffer, int length, int start_id, int to_start_id) {
+  int i;
+
+  for (i = 0; i<length; i++) 
+    memcpy(buffer + i * BLOCK_SIZE,
+	   buffer + where_it_is[start_id + i].location * BLOCK_SIZE, BLOCK_SIZE);
+
+  memcpy(where_it_is + to_start_id, where_it_is + start_id, 
+	 length * sizeof(instance_sort));
+}
+
+void one_sort_pass (char *input_file_name, char *output_file_name) {
+  int input, output;
+  char *buffer;
+  int half = 512;
+  int read, rest;
+  int start_id = 0;
+  int length;
+
+  if ((input = open64(input_file_name, O_RDONLY)) == -1)
+    merror("Opening the instance file");
+
+  if ((output = open64(output_file_name, O_RDWR|O_CREAT, 0644)) == -1)
+    merror("Opening the instance file");
+
+  buffer = cmalloc(half * BLOCK_SIZE * 2);
+  read_block(input, buffer, half * BLOCK_SIZE);
+  read = half;
+
+  do {
+    rest = min(read + half, current_instance_block_number);
+    length = rest-read;
+    read_block(input, buffer + half * BLOCK_SIZE, length * BLOCK_SIZE);
+    sort_buffer(half + length, start_id);
+    save_sorted(output, buffer, half, start_id);
+    move_sorted(buffer, length, start_id + half, start_id);
+    start_id += half;
+  } while (read == current_instance_block_number);
+
+  save_sorted(output, buffer + half * BLOCK_SIZE, length, start_id + half);
+
+  close(input);
+  close(output);
+}
+
+void defragment_sort (void) {
+  char *input = index_file_name(INSTANCE_FILE);
+  char *output1 = "/opt/tmp/instances1.tmp";
+  char *output2 = "/opt/tmp/instances2.tmp";
+  char *output = output1;
+  int i;
+
+  for (i = 0; i<7; i++) {
+    one_sort_pass(input, output);
+    if (i % 2) {
+      input = output2;
+      output = output1;
+    } else {
+      input = output1;
+      output = output2;
+    }
+  }
+}
